5f9d47153369adba538dfd696fb75909856a8830
debajit <debajit.sen2@gmail.com>
2022-05-16 18:43:34 +0530

    Fix war deployment issue

--- src/main/java/com/name/pronounciation/PronunciationApplication.java
+++ src/main/java/com/name/pronounciation/PronunciationApplication.java
@@ -2,6 +2,8 @@ package com.name.pronounciation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,7 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class PronunciationApplication {
+public class PronunciationApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PronunciationApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PronunciationApplication.class, args);
@@ -26,6 +33,5 @@ public class PronunciationApplication {
             }
         };
     }
-
 
 }
